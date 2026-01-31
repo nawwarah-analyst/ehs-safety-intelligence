b385c5a130e9f7bd8cd52f02102a41bc24b7a125
Nawwarah Yusof | Data Analyst (Power BI & BI) <nawwarahyusof98@gmail.com>
2026-01-31 22:25:13 +0800

    Create 01_schema.sql

---
+++ sql/01_schema.sql
@@ -0,0 +1,8 @@
+-- Raw tables from EHS system
+-- ehs_incidents
+-- ehs_training
+-- ehs_audits
+-- employees
+
+-- These tables contain messy operational data
+-- They are cleaned in 02_data_cleaning.sql
