f6ed6ea0f10b1c95ab7cf4cbea01eeae5ceba6a6
Nawwarah Yusof | Data Analyst (Power BI & BI) <nawwarahyusof98@gmail.com>
2026-01-31 22:47:57 +0800

    Create 05_kpi_metrics.sql

---
+++ sql/05_kpi_metrics.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TABLE practice-project-2025-9898.ehs_analytics.dim_department AS
+SELECT DISTINCT dept_name FROM (
+SELECT dept_name FROM practice-project-2025-9898.ehs_analytics.incidents_clean
+UNION DISTINCT
+SELECT dept_name FROM practice-project-2025-9898.ehs_analytics.audit_clean
+UNION DISTINCT
+SELECT dept_name FROM practice-project-2025-9898.ehs_analytics.training_clean
+)
+WHERE dept_name IS NOT NULL;
